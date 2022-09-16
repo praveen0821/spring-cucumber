28846f185fe6ba3d6d1b521987765b9a4da43458
praveen0821 <40320224+praveen0821@users.noreply.github.com>
2022-09-16 19:54:14 +0530

    Update BaeldungController.java

--- src/main/java/com/cucumber/controller/BaeldungController.java
+++ src/main/java/com/cucumber/controller/BaeldungController.java
@@ -12,7 +12,7 @@ public class BaeldungController {
         return "hello";
     }
 
-    @PostMapping("/baeldung")
+    @GetMapping("/baeldung")
     public String sayHelloPost() {
         return "hello";
     }
